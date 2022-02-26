b896dad3260d7be3073c8999a6003b74760552f6
venky <itsforvenky@gmail.com>
2022-02-27 00:37:58 +0530

    VM-instances code using terraform

---
+++ terraform-gcp/vm-instances/main.tf
@@ -0,0 +1,28 @@
+provider "google" {
+    version = "~> 4.0.0"
+    credentials = file("/devops-learning-26.json")
+    project = "devops-learning-26"
+    region = "us-central1"
+    zone = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+    name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+    name = "terraform-instance2"
+    machine_type = "f1-micro"
+    zone = "us-central1-c"
+    boot_disk {
+        initialize_params {
+            image = "centos-cloud/centos-7"
+        }
+    }
+
+    network_interface {
+        network = google_compute_network.vpc_network.name
+        access_config {
+        }
+    }
+}
